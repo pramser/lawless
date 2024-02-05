e5f53672a2d2e58638a2cc98bc223a61bc1b420a
Patrick Ramser <pramser@gmail.com>
2024-02-05 00:14:25 -0500

    Attach augments to side of window

--- app/item/[itemId]/page.tsx
+++ app/item/[itemId]/page.tsx
@@ -39,7 +39,7 @@ export default async function ItemDetail({ params: { itemId } }: any) {
       )}
 
       <h2 className="mb-2 mt-4 text-xl underline">Augments</h2>
-      <table className="border-separate border-spacing-2 border border-slate-500 w-2/4">
+      <table className="border-separate border-spacing-2 border border-slate-500 w-auto">
         <tbody>
           {augments.map((augment) => (
             <tr key={augment.id}>
