f49cef145b73c8363bc5013490fcf9082fa61d6a
Patrick Ramser <pramser@gmail.com>
2024-02-05 15:35:56 -0500

    Add some style to the header with Google web fonts

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,23 @@
 "use client"
 
+// next font
+import { Rock_Salt } from "next/font/google"
+import Link from "next/link"
+
+const rock_salt = Rock_Salt({
+  weight: "400",
+  subsets: ["latin"],
+})
+
 type HeaderParams = {}
 
 export default function Header({}: HeaderParams) {
   return (
     <header className="bg-gradient-to-r from-blue-700 to-indigo-900 flex flex-row px-4 py-2 items-center text-white w-fill">
-      <span className="cursor-default">lawless.gg</span>
+      <Link href="/" className="cursor-default text-2xl" style={rock_salt.style}>
+        lawless
+      </Link>
+      .gg
     </header>
   )
 }
