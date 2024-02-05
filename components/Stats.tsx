d8730a2bef363d9f1bd79c71469d72ae8a660f00
Patrick Ramser <pramser@gmail.com>
2024-02-05 17:49:09 -0500

    Basic stats table for detail page

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,7 +1,20 @@
+import { JsonObject } from "@prisma/client/runtime/library"
+
 interface StatsProps {
-  stats: any
+  stats: JsonObject
 }
 
 export default function Stats({ stats }: StatsProps) {
-  return <p>Placeholder for stats!</p>
+  return (
+    <table className="bg-black bg-opacity-40 border-separate border-spacing-2 border border-slate-500 w-auto">
+      <tbody>
+        {Object.keys(stats).map((key, i) => (
+          <tr key={i} className="text-sm">
+            <td>{key}</td>
+            <td>{stats[key]?.toString()}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
