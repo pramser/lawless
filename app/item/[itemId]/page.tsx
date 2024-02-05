9521e6290aca77b06c15de17ee47554eaea336a7
Patrick Ramser <pramser@gmail.com>
2024-02-05 11:31:55 -0500

    Update augments to possible augments

--- app/item/[itemId]/page.tsx
+++ app/item/[itemId]/page.tsx
@@ -51,7 +51,7 @@ export default async function ItemDetail({ params: { itemId } }: any) {
         </div>
       )}
 
-      <h2 className="mb-2 mt-4 text-xl underline">Augments</h2>
+      <h2 className="mb-2 mt-4 text-xl underline">Possible Augments</h2>
       <table className="bg-black bg-opacity-40 border-separate border-spacing-2 border border-slate-500 w-auto">
         <tbody>
           {augments.map((augment) => (
